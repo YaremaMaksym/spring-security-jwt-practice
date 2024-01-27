f39eeb166cac19f29aaea88a0c6d175191bb2337
Maksym Yarema <macsym2@gmail.com>
2024-01-27 15:51:59 +0200

    update securityFilterChain(): permit all methods to "api/v1/auth" path. Because there are only authentication related methods and no business logic

--- src/main/java/org/yaremax/springsecurityjwtpractice/config/SecurityConfiguration.java
+++ src/main/java/org/yaremax/springsecurityjwtpractice/config/SecurityConfiguration.java
@@ -24,7 +24,7 @@ public class SecurityConfiguration {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authz -> authz
-//                        .requestMatchers("/**").permitAll()
+                        .requestMatchers("/api/v1/auth/**").permitAll()
                         .anyRequest().authenticated())
                 .sessionManagement(s -> s
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
