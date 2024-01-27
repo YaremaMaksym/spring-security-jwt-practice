7789e835e68b947bfbc1c905ade13cc15ec1c204
Maksym Yarema <macsym2@gmail.com>
2024-01-27 10:10:05 +0200

    init project

---
+++ src/test/java/org/yaremax/springsecurityjwtpractice/SpringSecurityJwtPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yaremax.springsecurityjwtpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJwtPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
