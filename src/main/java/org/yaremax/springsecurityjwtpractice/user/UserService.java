9debfba4b093dc5aafd41112eb64388bbdefc6dd
Maksym Yarema <macsym2@gmail.com>
2024-01-27 15:46:16 +0200

    add addUser() method to UserService

--- src/main/java/org/yaremax/springsecurityjwtpractice/user/UserService.java
+++ src/main/java/org/yaremax/springsecurityjwtpractice/user/UserService.java
@@ -1,5 +1,6 @@
 package org.yaremax.springsecurityjwtpractice.user;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,5 +17,13 @@ public class UserService {
     public UserEntity findUserByEmail(String email) {
         return userRepository.findByEmail(email)
                 .orElseThrow(() -> new IllegalArgumentException("User with email " + email + " wasn't found"));
+    }
+
+    @Transactional
+    public void addUser(UserEntity user) {
+        if (userRepository.existsByEmail(user.getEmail())) {
+            throw new IllegalArgumentException("User with email " + user.getEmail() + " already exists");
+        }
+        userRepository.save(user);
     }
 }
