19f557efb59a07d61649d3b5c36b008cbd245e80
Maksym Yarema <macsym2@gmail.com>
2024-01-27 15:16:57 +0200

    add Spring Security configurations(JwtAuthenticationFilter, JwtService, ApplicationConfig, SecurityConfiguration)

---
+++ src/main/java/org/yaremax/springsecurityjwtpractice/config/ApplicationConfig.java
@@ -0,0 +1,43 @@
+package org.yaremax.springsecurityjwtpractice.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.yaremax.springsecurityjwtpractice.user.UserDetailsImpl;
+import org.yaremax.springsecurityjwtpractice.user.UserService;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+    private final UserService userService;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> new UserDetailsImpl(userService.findUserByEmail(username));
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
