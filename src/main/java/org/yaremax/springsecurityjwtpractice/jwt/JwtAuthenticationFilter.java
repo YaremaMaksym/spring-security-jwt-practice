5f8065d1389e848b67ccecd1c4b000452bbb3095
Maksym Yarema <macsym2@gmail.com>
2024-01-29 14:30:36 +0200

    ref(JwtAuthenticationFilter): moved initialization of String jwt variable

--- src/main/java/org/yaremax/springsecurityjwtpractice/jwt/JwtAuthenticationFilter.java
+++ src/main/java/org/yaremax/springsecurityjwtpractice/jwt/JwtAuthenticationFilter.java
@@ -29,19 +29,18 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain) throws ServletException, IOException {
         final String authHeader = request.getHeader("Authorization"); //header that contains jwt token
-        final String jwt;
 
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
             filterChain.doFilter(request, response);
             return;
         }
 
-        jwt = authHeader.substring(7);
+        final String jwt = authHeader.substring(7);
         final String subject = jwtService.extractSubject(jwt);
 
         if (subject != null
                 && SecurityContextHolder.getContext().getAuthentication() == null) {
-            UserDetails userDetails = this.userDetailsService.loadUserByUsername(subject);
+            UserDetails userDetails = userDetailsService.loadUserByUsername(subject);
 
             if (jwtService.isTokenValid(jwt, userDetails)) {
                 UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
